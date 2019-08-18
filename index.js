376ba7d2a954cba9ca47354691d4c3f91435e3c4
Damitha Perera <dami.sanka@gmail.com>
2019-08-18 18:02:19 +0530

    connect to mongoose

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+//connect to db
+mongoose.connect( process.env.DB_CONNECT,   { useNewUrlParser: true},   ()=> { console.log('connected to db')});
 
 //Import Routes
 const authRoute = require('./routes/auth');
